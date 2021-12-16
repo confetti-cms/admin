5b45e7c82083edd006ed0e0b324bddc5625811d0
Andrew Boyd <andrew@wearebraid.com>
2021-12-16 13:32:37 -0500

    update starter project to use alpha-4
    Signed-off-by: Andrew Boyd <andrew@wearebraid.com>

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { plugin, defaultConfig } from '@formkit/vue'
-import '@formkit/themes/dist/genesis/index.esm.css'
+import '@formkit/themes/genesis/theme.css'
 
 createApp(App).use(plugin, defaultConfig).mount('#app')
