2769edc4f6f7c1ab4e477b031866e3bf6977de15
Luan Nguyen <luan@wearebraid.com>
2022-03-31 12:03:10 -0400

    Fixes Genesis theme import and bumps versions in the package.json.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { plugin, defaultConfig } from '@formkit/vue'
-import '@formkit/themes/genesis/theme.css'
+import '@formkit/themes/genesis'
 
 createApp(App).use(plugin, defaultConfig).mount('#app')
