8199003ecf0c6b7ca91a7c796a33afcd04d9b634
Reindert Vetter <reindertvetter@gmail.com>
2022-10-11 00:27:17 +0200

    Render form

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,110 @@
 <template>
   <div class="container">
     <div class="sample-input">
-      <h1>Introduction</h1>
       <FormKit type="form" v-model="formData" v-if="schema">
-        <FormKitSchema :schema="schema" :data="formData"/>
+        <FormKitSchema :schema="schema" :data="formData" />
       </FormKit>
 
-      <h2>Modeled group values</h2>
-      <pre class="form-data">{{ formData }}</pre>
+      <!-- <h2>Modeled group values</h2> -->
+      <!-- <pre class="form-data">{{ formData }}</pre> -->
     </div>
   </div>
 </template>
 
 <script>
-import {ref} from 'vue'
-import allComponents from '/Users/reindertvetter/dev/confetti-cms/render-form/storage/components/views/index.blade.php.json'
+import { ref } from "vue";
+import allComponents from "/Users/reindertvetter/dev/confetti-cms/render-form/storage/components/views/home.blade.php.json";
 
 function mapComponentsToSchema(components, groupId) {
-  let keySuffix = '';
+  let keySuffix = "";
   if (groupId !== null) {
-    keySuffix = keySuffix + '-' + groupId;
+    keySuffix = keySuffix + "-" + groupId;
   }
 
   let schema = [];
   components.forEach((component) => {
-
     let schemaItem;
 
-    if (component.type === 'multiple') {
-      for (let i = 0; i < 2; i++) {
+    if (component.type === "multiple") {
+      schema.push({
+        $el: "h3",
+        children: component.label,
+      });
+      for (let i = 0; i < 3; i++) {
         {
           let oneOfMultipleGroupId = uuid();
           let belongsTo = {
-            $formkit: 'hidden',
-            name: component.key + '.belongs_to-' + oneOfMultipleGroupId,
-            value: groupId
+            $formkit: "hidden",
+            name: component.key + ".belongs_to-" + oneOfMultipleGroupId,
+            value: groupId,
           };
           schemaItem = {
-            $el: 'fieldset',
+            $el: "fieldset",
             children: [
               belongsTo,
-              ...mapComponentsToSchema(find(allComponents, component.key), oneOfMultipleGroupId)
-            ]
-          }
-          schema.push(schemaItem)
+              ...mapComponentsToSchema(
+                find(allComponents, component.key),
+                oneOfMultipleGroupId
+              ),
+            ],
+          };
+          schema.push(schemaItem);
         }
       }
     } else {
-      let label = component.label !== '' ? component.label : component.key;
+      let label = component.label !== "" ? component.label : component.key;
+      let isEl = isElement(component);
+      let hasChildren = component.type != "select";
 
-      let isEl = isElement(component)
-      schemaItem = {
-        ...isEl && {$el: getElement(component)},
-        ...!isEl && {$formkit: getFormKitType(component)},
-        label: label,
-        name: component.key + keySuffix,
-        placeholder: component.placeholder !== '' ? component.placeholder : label,
-        help: component.help,
-        validation: getValidation(component),
-        children: mapComponentsToSchema(find(allComponents, component.key), groupId),
-      };
+      if (component.type == "select" && component.default == "") {
+        component.default = component.options[0];
+      }
 
-      schema.push(schemaItem)
+      if (isEl) {
+        if (component.type == "section") {
+          schema.push({
+            $el: "hr",
+            attrs: {
+              style: {
+                "margin-top": "30px",
+                "border": "1px solid gray",
+              },
+            },
+          });
+        }
+        schema.push({
+          $el: getElement(component),
+          children: [component.label],
+        });
+        schema.push({
+          $el: "div",
+          children: mapComponentsToSchema(
+            find(allComponents, component.key),
+            groupId
+          ),
+        });
+      } else {
+        schema.push({
+          $formkit: getFormKitType(component),
+          label: label,
+          value: component.default,
+          name: component.key + keySuffix,
+          placeholder:
+            component.placeholder !== "" ? component.placeholder : label,
+          options: component.options,
+          help: component.help,
+          validation: getValidation(component),
+          ...(hasChildren && {
+            children: mapComponentsToSchema(
+              find(allComponents, component.key),
+              groupId
+            ),
+          }),
+          validationVisibility: "live",
+        });
+      }
     }
-  })
+  });
 
   return schema;
 }
@@ -73,91 +114,104 @@ function find(collection, key) {
     if (elem.parent_key === key) {
       return elem;
     }
-  })
-}
-
-function getElement(component) {
-  if (component.type === 'section') {
-    return "div";
-  }
-  if (component.type === '') {
-    return "div";
-  }
-
-  return null;
+  });
 }
 
 function isElement(component) {
   switch (component.type) {
-    case 'section':
-      return 'dev';
-    case 'localFile':
-      return 'h1';
+    case "section":
+      return true;
+    case "localFile":
+      return true;
     default:
-      return false
+      return false;
+  }
+}
+
+function getElement(component) {
+  switch (component.type) {
+    case "localFile":
+      return "h1";
+    case "section":
+      return "h2";
+    default:
+      return false;
   }
 }
 
 function getValidation(component) {
-  let validations = []
-  if (component.min_apply) {
-    validations.push('min:' + component.min);
-  }
-  if (component.max_apply) {
-    validations.push('max:' + component.max);
+  let validations = [];
+
+  if (component.type == "text" || component.type == "textarea") {
+    if (component.min_apply) {
+      validations.push("minlength:" + component.min);
+    }
+    if (component.max_apply) {
+      validations.push("maxlength:" + component.max);
+    }
+  } else {
+    if (component.min_apply) {
+      validations.push("min:" + component.min);
+    }
+    if (component.max_apply) {
+      validations.push("max:" + component.max);
+    }
   }
   if (component.required) {
-    validations.push('required');
+    validations.push("required");
   }
 
-  return validations.join('|');
+  return validations.join("|");
 }
 
 function getFormKitType(component) {
   switch (component.type) {
-    case 'checkbox':
-      return 'checkbox';
-    case 'color':
-      return 'color';
-    case 'date':
-      return 'date';
-    case 'dateTime':
-      return 'datetime-local';
-    case 'email':
-      return 'email';
-    case 'image':
+    case "checkbox":
+      return "checkbox";
+    case "color":
+      return "color";
+    case "date":
+      return "date";
+    case "dateTime":
+      return "datetime-local";
+    case "email":
+      return "email";
+    case "image":
       return "file";
-    case 'month':
-      return 'month';
-    case 'number':
+    case "month":
+      return "month";
+    case "number":
       return "number";
-    case 'radio':
-      return 'radio';
-    case 'range':
-      return 'range';
-    case 'select':
-      return 'select';
-    case 'telephone':
-      return 'tel';
-    case 'text':
+    case "radio":
+      return "radio";
+    case "range":
+      return "range";
+    case "select":
+      return "select";
+    case "telephone":
+      return "tel";
+    case "text":
       return "text";
-    case 'textarea':
-      return 'textarea';
-    case 'time':
-      return 'time';
-    case 'url':
-      return 'url';
-    case 'localFile':
-      return 'h1';
+    case "textarea":
+      return "textarea";
+    case "time":
+      return "time";
+    case "url":
+      return "url";
+    case "localFile":
+      return "h1";
     default:
-      console.warn('type: ' + component.type)
+      console.warn("type: " + component.type);
       return "unknown";
   }
 }
 
 function uuid() {
-  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
-      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+    (
+      c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+    ).toString(16)
   );
 }
 
@@ -165,22 +219,20 @@ export default {
   setup() {
     const schema = ref(null);
 
-    let rootComponents = find(allComponents, "")
+    let rootComponents = find(allComponents, "");
 
     schema.value = mapComponentsToSchema(rootComponents, uuid());
 
-    console.debug(JSON.parse(JSON.stringify(schema.value)))
-
-    const recommendation = ref('10')
-    const formData = ref({})
+    const recommendation = ref("10");
+    const formData = ref({});
 
     return {
       recommendation,
       schema,
-      formData
-    }
+      formData,
+    };
   },
-}
+};
 </script>
 
 
