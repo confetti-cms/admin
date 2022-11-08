a89abaf41a162aa6f592d25c489c1631e292a958
barthaasbroek <bart@phoenixsite.nl>
2022-11-08 22:24:17 +0100

    change admin styling

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,39 @@
 <template>
+  <div class="admin-panel">
+    <div class="admin-panel__sidebar admin-sidebar">
+      <div class="admin-sidebar__logo">
+        <img src="/placeholder-logo.png" alt="" />
+      </div>
+      <nav class="admin-sidebar__navigation">
+        <ul>
+          <li><a href="#" class="is-active">Home</a></li>
+          <li><a href="#">Pagina's</a></li>
+          <li><a href="#">Berichten</a></li>
+          <li><a href="#">Projecten</a></li>
+          <li><a href="#">Weergave</a></li>
+          <li><a href="#">Instellingen</a></li>
+        </ul>
+      </nav>
+      <div class="admin-sidebar__hamburger-menu" @click="">
+        <span></span>
+      </div>
+      <div class="admin-sidebar__mobile-navigation"></div>
+    </div>
+    <div class="admin-panel__content">
+      <FormKit type="form" v-model="formData" v-if="schema">
+        <FormKitSchema :schema="schema" :data="formData" />
+      </FormKit>
+    </div>
+  </div>
   <div class="container">
-    <div class="sample-input">
+    <!-- <div class="sample-input">
       <FormKit type="form" v-model="formData" v-if="schema">
         <FormKitSchema :schema="schema" :data="formData" />
       </FormKit>
 
-      <!-- <h2>Modeled group values</h2> -->
-      <!-- <pre class="form-data">{{ formData }}</pre> -->
-    </div>
+      <h2>Modeled group values</h2>
+      <pre class="form-data">{{ formData }}</pre>
+    </div> -->
   </div>
 </template>
 
@@ -67,7 +93,7 @@ function mapComponentsToSchema(components, groupId) {
             attrs: {
               style: {
                 "margin-top": "30px",
-                "border": "1px solid gray",
+                border: "1px solid gray",
               },
             },
           });
@@ -235,15 +261,147 @@ export default {
 };
 </script>
 
-
 <style>
+.admin-panel {
+  display: grid;
+  grid-template-columns: 20% 1fr;
+  gap: 20px;
+}
+.admin-panel__sidebar {
+  background: #f1f1f1;
+  padding: 20px;
+  position: sticky;
+  top: 0;
+  height: 100vh;
+}
+.admin-panel__content {
+  background: #f1f1f1;
+  padding: 20px;
+}
+
+.admin-sidebar__hamburger-menu {
+  display: none;
+  width: 28px;
+  height: 20px;
+  position: absolute;
+  right: 20px;
+}
+
+.admin-sidebar__hamburger-menu:before,
+.admin-sidebar__hamburger-menu:after,
+.admin-sidebar__hamburger-menu span {
+  display: block;
+  content: "";
+  height: 2px;
+  background: white;
+  width: 100%;
+  position: absolute;
+  z-index: 12;
+}
+
+.admin-sidebar__hamburger-menu:after {
+  bottom: 0;
+}
+
+.admin-sidebar__hamburger-menu span {
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+.admin-sidebar__mobile-navigation {
+  width: 90%;
+  position: fixed;
+  right: 0;
+  right: -100%;
+  height: 100vh;
+  top: 0;
+  z-index: 10;
+  background: white;
+  transition: right 0.4s ease;
+}
+
+.admin-sidebar__navigation {
+  margin-top: 20px;
+}
+
+.admin-sidebar__navigation ul {
+  padding: 0;
+  margin: 0;
+}
+.admin-sidebar__navigation ul li {
+  list-style-type: none;
+}
+.admin-sidebar__navigation ul li + li {
+  margin-top: 10px;
+}
+.admin-sidebar__navigation ul li a {
+  text-decoration: none;
+  padding: 8px 10px;
+  transition: background 0.4s ease;
+  border-radius: 6px;
+  display: inline-block;
+  color: #1d1d1d;
+  font-weight: 500;
+}
+.admin-sidebar__navigation ul li a:hover,
+.admin-sidebar__navigation ul li a.is-active {
+  background: #ccc;
+}
+
+@media only screen and (max-width: 600px) {
+  .admin-panel {
+    grid-template-columns: 1fr;
+    grid-template-rows: 50px 1fr;
+    gap: 0;
+  }
+  .admin-panel__sidebar {
+    background: #333;
+    position: sticky;
+    top: 0;
+    z-index: 10;
+    padding-top: 0;
+    padding-bottom: 0;
+    height: 100%;
+    display: flex;
+    align-items: center;
+  }
+  .admin-sidebar__logo {
+    height: 50px;
+  }
+  .admin-sidebar__logo img {
+    max-height: 100%;
+  }
+  .admin-sidebar__navigation {
+    display: none;
+  }
+  .admin-sidebar__hamburger-menu {
+    display: block;
+  }
+  .admin-panel__content {
+    padding-top: 0;
+  }
+}
+
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+img {
+  display: block;
+  max-width: 100%;
+}
 /*noinspection CssUnusedSymbol*/
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
 .container {
