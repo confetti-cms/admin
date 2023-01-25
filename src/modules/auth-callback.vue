c8d84ca57e2dd1dfbbec58a5711d25a3ee1708e3
Reindert Vetter <reindertvetter@gmail.com>
2023-01-26 00:11:15 +0100

    Log in by auth service

---
+++ src/modules/auth-callback.vue
@@ -0,0 +1,67 @@
+<script>
+//import axios from 'axios'
+
+export default {
+    data() {
+        return {}
+    },
+    methods: {
+        getAccessToken: function (event) {
+
+            const queryString = window.location.search;
+            console.log(queryString);
+
+            const urlParams = new URLSearchParams(queryString);
+            const code = urlParams.get('code')
+            console.log(code);
+
+            const state = urlParams.get('state')
+            console.log(state);
+
+
+            // @todo, Use httpOnly cookie. The server can't set the cookie for now.
+            const Http = new XMLHttpRequest();
+            const url = 'http://localhost:3010/api/callback?code=' + code + "&state=" + state;
+            Http.open("GET", url);
+            Http.send();
+
+            Http.onreadystatechange = (e) => {
+                console.log("status", Http.status)
+                const response = JSON.parse(Http.responseText)
+                console.log("response", response)
+                alert("You (" + response["profile"]["name"] + ") are logged. To prove this, we make another request to retrieve your data.");
+
+                const accessToken = response["auth"]["access_token"]
+                console.log(accessToken)
+                document.cookie = "access_token=" + accessToken;
+
+                // Get user profile
+                const url = 'http://localhost:3010/api/users/me';
+                Http.open("GET", url);
+                Http.setRequestHeader("Cookie", document.cookie)
+                Http.send();
+
+                Http.onreadystatechange = (e) => {
+                    console.log("status", Http.status)
+                    const response = JSON.parse(Http.responseText)
+                    console.log("response", response)
+                    alert(response["name"] + " from second request");
+                }
+            }
+        }
+    }
+}
+</script>
+
+<template>
+    This is the callback page.
+
+    <br>
+    <br>
+    <button @click="getAccessToken" style="color:green">Click here (in the feature, no click needed)</button>
+
+    <br>
+    <br>
+    When 401, retry this flow
+
+</template>
---
+++ src/modules/login.vue
@@ -0,0 +1,39 @@
+<script>
+//import axios from 'axios'
+
+export default {
+    data() {
+        return {}
+    },
+    methods: {
+        login(event) {
+        // Helaas, ik kon axios niet importeren
+//            axios
+//                .get('http://localhost:3010/api/login')
+//                .then(response => {
+//                    console.log(response.data)
+//                })
+//                .catch(error => {
+//                    console.log(error)
+//                })
+//                .finally(() => this.loading = false)
+
+
+            const Http = new XMLHttpRequest();
+            const url = 'http://localhost:3010/api/login';
+            Http.open("GET", url);
+            Http.send();
+
+            Http.onreadystatechange = (e) => {
+                const response = JSON.parse(Http.responseText)
+                const redirectUrl = response["redirect_url"];
+                window.location.replace(redirectUrl);
+            }
+        }
+    }
+}
+</script>
+
+<template>
+    <button @click="login">Go to login page</button>
+</template>
